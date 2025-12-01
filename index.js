e2d7a1827650f3ed6cc52c74561a4cdbda43cb71
Vinícius Dias <vyvy292004@gmail.com>
2025-12-01 02:25:49 -0300

    feat: initiates the creation of a post for users

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import express from 'express'
 import { PostgresHelper } from './src/db/postgres/helper.js'
 
 const app = express()
-
+app.use(express.json())
 app.get('/', async (req, res) => {
     const result = await PostgresHelper.query('SELECT * FROM users')
     res.send(JSON.stringify(result.rows))
 })
 
-app.listen(3000, () => console.log('Teste'))
+app.post('/api/users', async (req, res) => {
+    console.log(req.body)
+    res.status(201).send('Usuário criado com sucesso')
+})
+
+app.listen(process.env.PORT, () => console.log('Teste'))
