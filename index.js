2de9fe93dd9aeaab0b5303bb6e711bc4cb0152be
Vinícius Dias <vyvy292004@gmail.com>
2025-11-30 21:14:33 -0300

    chore: setups pg and dotenv

---
+++ index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config.js'
+import express from 'express'
+import { PostgresHelper } from './src/db/postgres/helper.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const result = await PostgresHelper.query('SELECT * FROM users')
+    res.send(JSON.stringify(result.rows))
+})
+
+app.listen(3000, () => console.log('Teste'))
